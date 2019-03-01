535d1950ee718e3d38727d29eb3c5aaa65d6e378
Sippakorn1 <46592484+Sippakorn1@users.noreply.github.com>
2019-03-01 15:47:33 +0700

    Add files via upload

---
+++ pracex2_3.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string>
+
+using namespace std;
+char table[8][8];
+
+void showTable();
+void randomTable();
+
+int main(){
+	srand(time(0));	
+	randomTable();
+	showTable();
+}
+
+void showTable(){
+	for(int i = 0; i < 8; i++){
+		for(int j = 0; j < 8; j++){
+			cout << table[i][j] << " ";
+		}
+		cout << "\n";
+	}
+}
+
+//Write definition of function randomTable() here.
+void randomTable(){
+	
+ 	for(int i = 0; i < 8; i++){
+		for(int j = 0; j < 8; j++){
+		char x =('A'+rand()%26);
+		cout <<  x <<" ";
+		
+		}
+		cout << "\n";
+	 }
+}
---
+++ pracex2_4.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+//Write prototype of  sg() here.
+double sg(double x,double y);
+
+int main(){
+    cout << sg(-2.5,1.1) << "\n";
+    cout << sg(1,10000) << "\n";
+    cout << sg(8.5,2.4) << "\n";
+    cout << sg(0.1,0.0001) << "\n";
+    cout << sg(10,0.007) << "\n";
+    cout << sg(1,-2) << "\n";
+    cout << sg(15,100) << "\n";
+    cout << sg(123,5432) << "\n";
+}
+
+//Write function definition of  sg() here.
+double sg(double x,double y){
+    double a,d,max,min;
+    if(x<=0 || y<=0){
+        a=0;
+        return a;
+    }   
+    else if(x>y){
+        max=x,min=y;
+        d=max/min;
+        }   
+        else{
+        max=y,min=x;
+        d=max/min;
+        }
+    if(d<=10){
+        a=x*y;
+        double e= sqrt(a);
+        return e;
+    }
+    else{
+        double b=sqrt(x*y),c=sg(x,b)+sg(y,b);
+        return c;
+    }
+   
+}   
